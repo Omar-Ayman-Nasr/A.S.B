74084e170fae98da8c522078b453e6a8dfa84b83
Omar-Ayman-Nasr <omar.aaelessawy@gmail.com>
2021-04-16 18:15:18 +0200

    Initial commit

---
+++ Student_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+
+// Students strcture
+struct Students
+{
+     char Name[20];
+	 int ID;
+	 int BD[3];
+	 int Score;
+}
+ int main ( int )
+ {
+	printf("Welcome\n"); 
+ }
+
